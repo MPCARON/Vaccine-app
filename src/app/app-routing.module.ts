915bb1acc70703006a7d651c2625a85f9c8ba510
Marie-Pier Caron <caron.marie.pier@gmail.com>
2021-04-22 22:03:10 -0400

    Premiere iteration Tp2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AcceuilComponent} from './acceuil/acceuil.component';
+import {MaladieComponent} from './maladie/maladie.component';
+import {PreventionComponent} from './prevention/prevention.component';
+import {TestComponent} from './test/test.component';
+import {VaccinComponent} from './vaccin/vaccin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'acceuil', component: AcceuilComponent},
+  {path: 'maladie', component: MaladieComponent},
+  {path: 'prevention', component: PreventionComponent},
+  {path: 'test', component: TestComponent},
+  {path: 'vaccin', component: VaccinComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
